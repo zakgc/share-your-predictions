e2e9c80d2bf936da7657dce49c8fe6685bd42bcc
zakgc <zakgriffithcrowe@gmail.com>
2026-03-10 21:27:29 +0000

    Add oscars types

---
+++ types/category.ts
@@ -0,0 +1,6 @@
+import { type Option } from './option'
+
+export type Category = {
+    name: string
+    options: Option[]
+}
---
+++ types/option.ts
@@ -0,0 +1,4 @@
+export type Option = {
+    option: string
+    details: string
+}
