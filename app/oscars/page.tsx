e8614a3513ce29302c754be1bf361416fed7668f
zakgc <zakgriffithcrowe@gmail.com>
2026-03-11 17:46:28 +0000

    Add functionality to create predictions object

--- app/oscars/page.tsx
+++ app/oscars/page.tsx
@@ -1,25 +1,70 @@
 "use client"
 
 const oscarsData = require('../../oscars.json')
-import { Category, Option } from "@/types";
+import { Category, Option, UserPrediction } from "@/types";
 import { shareTextToWhatsApp } from "share-text-to-whatsapp";
 
 export default function Home() {
-  let predictions = 'Nothing'
+  let userPredicitons: UserPrediction[] = []
+  
+  oscarsData.categories.forEach((category: Category) => {
+    userPredicitons.push({
+      category: category.name,
+      prediction: ''
+    })
+  });
+
+  const addPrediction = (categoryName: string, prediction: string) => {
+    let predicitonIndex = userPredicitons
+      .findIndex((prediction) => prediction.category === categoryName)
+
+    userPredicitons[predicitonIndex] = {
+      category: categoryName,
+      prediction: prediction
+    }
+  }
+
+  const formatPredicitions = (): string => {
+    let filteredUserPredictions = userPredicitons.filter((prediction) => {
+      return prediction.prediction !== ''
+    })
+
+    let formattedPredicitions = 'Oscars 2026 Predictions'
+
+    filteredUserPredictions.forEach((prediction) => {
+      let predictionText = `\n*${prediction.category}*\n${prediction.prediction}`
+
+      formattedPredicitions += predictionText
+    })
+
+    return formattedPredicitions
+  }
+
+  const sharePredicitions = () => {
+    let text = formatPredicitions()
+
+    shareTextToWhatsApp(text)
+  }
+
   return (
     <main>
       <h1>What are your predicitons for the Oscars</h1>
       <br/>
-      {oscarsData.categories.map((category: Category, index: number) => {
-        let categoryKey = `${category.name}-${index}`
+      {oscarsData.categories.map((category: Category, categoryIndex: number) => {
+        let categoryKey = `${category.name}-${categoryIndex}`
         return (
           <div key={categoryKey}>
             <h1>{category.name}</h1>
             <br/>
-            {category.options.map((option: Option, index: number) => {
-              let optionKey = `${category.name}-${option.option}-${index}`
+            {category.options.map((option: Option, optionIndex: number) => {
+              let optionKey = `${category.name}-${option.option}-${optionIndex}`
+              let optionId = `${categoryIndex}-${optionIndex}`
               return (
-                <div key={optionKey}>
+                <div
+                  id={optionId}
+                  key={optionKey}
+                  onClick={() => {addPrediction(category.name, option.option)}}
+                >
                   <h2>{option.option}</h2>
                   <h3>{option.details}</h3>
                 </div>
@@ -30,7 +75,7 @@ export default function Home() {
         )
       })}
 
-      <button onClick={() => {shareTextToWhatsApp(predictions)}}>
+      <button onClick={() => {sharePredicitions()}}>
         Share to Whatsapp
       </button>
     </main>
